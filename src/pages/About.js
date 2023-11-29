3928009a4f010efc219bf0bef20f4f8b444e4cf1
mgwall17 <mgwall17@gmail.com>
2023-11-28 17:52:57 -0700

    added content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,44 @@
+/* eslint-disable react/no-unescaped-entities */
 "use client";
 import { Link } from "@chakra-ui/next-js";
-import { Container, Heading, Text } from "@chakra-ui/react";
+import { Container,Box,Center, Heading, Text, VStack } from "@chakra-ui/react";
 export default function About() {
   return (
     <div>
       <Container>
-        <Heading>Jose Zaragoza</Heading>
-        <Text mt={2}>
-          As your host, I will be your guide through the horror glass. I am a master of horror and delight.
-        </Text>
+        <Center>
+        <VStack spacing={4} align='stretch' mt={[8,2,2]} p={[0,4,8]} w={['100vw', '70vw', '90vw']}>
+        <Box>
+        <Heading mb={4} size="lg">About the Host</Heading>
+        <Text>
+          My name is Jose Zaragoza. Nice to meet you! </Text>
+          <Text mt={2}>I'm a recent graduate of the Masters in Digital Marketing program at Arizona State University. Armed with a passion for horror and a freshly minted degree, I've embarked on an expedition to bring you the spine-chilling wonders of my Horror Glass Podcast.
+          </Text>
+          </Box>
+          <Box>
+          <Heading mb={4} size="lg">Why I made Horror Glass</Heading>
+            <Text>
+            Fostering a conversation with fellow avid fans isn't just about discussing scary movies; it's a shared exploration of the visceral and psychological impact that these films have on individuals and society. By engaging in conversations with fellow enthusiasts, we unearth personal experiences, diverse perspectives, and the nuanced emotions that make horror a unique and captivating genre. My podcast serves as a communal space where the love for horror is celebrated, fostering a sense of connection and understanding among listeners. Beyond being a platform for entertainment, Horror Glass Podcast becomes a shared journey through the realms of fear, where the passion for the genre takes center stage, offering listeners a chance to revel in the thrills and chills that unite horror fans worldwide.
+          </Text>
+          </Box>
+          <Box>
+          <Heading mb={4} size="lg">Why everyone should make a Podcast</Heading>
+            <Text>
+            Aside from just being plain fun, creating a podcast is a multifaceted learning journey. It sharpens communication skills through articulate expression and engaging interview techniques, fostering the ability to convey ideas compellingly. The process enhances project management capabilities as students plan, execute, and maintain a consistent podcast schedule, emphasizing organization and time management. Crafting content and narratives for each episode develops creative thinking, a skill integral to digital marketing. Podcasting also serves as a hands-on experience in content creation, from scriptwriting to audio editing, honing storytelling abilities. It gives me practice on building and maintaining an online presence through social media, providing practical insights into audience engagement and brand building. In essence, podcast creation becomes a holistic learning experience, blending creativity, communication, organization, and analytics, all essential facets of a well-rounded digital marketing skill set.
+          </Text>
+          </Box>
+          <Box>
+            <Text>
+          Connect with me on Sound Cloud or LinkedIn to stay updated on the latest episodes, behind-the-scenes shenanigans, and perhaps some marketing insights thrown in for good measure.
+          </Text>
+          </Box>
+          <Box>
+          <Text>
+          Thank you for being a part of the Horror Glass journey!
+          </Text>
+          </Box>
+        </VStack>
+        </Center>
       </Container>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Inter } from 'next/font/google'
 import { Heading,Hide, Center,HStack, StackDivider, VStack, Stack, Text, Box } from '@chakra-ui/react'
 import { MoonIcon } from '@chakra-ui/icons'
@@ -24,7 +25,7 @@ export default function Home() {
           <Heading align={'center'} mb={2} as="p" size="2xl">Calling all horror fans!
           </Heading> 
             <Text fontSize='2xl' align={'center'} >
-           My name is Jose Zaragoza, and as a fellow lover of all things eerie, I am thrilled to invite you on a journey through the world of horror movies. In each episode, I sit down with a random guest who, like you and me, has been profoundly impacted by a particular horror film. Together, we unravel the psychological layers that make these movies so captivating.
+           My name is Jose Zaragoza, and as a fellow lover of all things eerie, I'm thrilled to invite you on a journey through the world of horror movies. In each episode, I sit down with a random guest who, like you and me, has been profoundly impacted by a particular horror film. Together, we unravel the psychological layers that make these movies so captivating.
           </Text>
           </Stack>
           </Center>
@@ -39,7 +40,7 @@ export default function Home() {
           <Box>
           <HStack><MoonIcon color={"orange"} /><Heading mb={2} as="h3" size="md">Unveiling Personal Fears</Heading></HStack>
           <Text>
-            Horror Glass is all about unmasking the fears that resonate on a personal level. I am here to share the untold stories behind the screams and explore the ways in which these films have left an indelible mark on the lives of my guests—and, hopefully, on yours too.
+            Horror Glass is all about unmasking the fears that resonate on a personal level. I'm here to share the untold stories behind the screams and explore the ways in which these films have left an indelible mark on the lives of my guests—and, hopefully, on yours too.
           </Text>
           </Box>
           <Box>
@@ -51,13 +52,13 @@ export default function Home() {
           <Box>
           <HStack><MoonIcon color={"orange"} /><Heading mb={2} as="h3" size="md">Never Miss a Terrifying Tale</Heading></HStack>
           <Text>
-            Do not want to miss out on the scares and insights? Subscribe to Horror Glass Podcast and join me each month for a new guest, a new movie, and a fresh exploration of the psychological terrain that makes horror cinema so uniquely captivating.
+            Don't want to miss out on the scares and insights? Subscribe to Horror Glass Podcast and join me each month for a new guest, a new movie, and a fresh exploration of the psychological terrain that makes horror cinema so uniquely captivating.
           </Text>
           </Box>
           <Box>
           <HStack><MoonIcon color={"orange"} /><Heading mb={2} as="h3" size="md">Step into Nightmares and Beyond</Heading></HStack>
           <Text>
-            Are you ready to step into the nightmares of our guests? Join the conversation on Horror Glass Podcast, where the chills are as real as the stories we unravel. The shadows are beckoning, and the stories are about to unfold— do not miss out on this journey into the heart of horror.
+            Are you ready to step into the nightmares of our guests? Join the conversation on Horror Glass Podcast, where the chills are as real as the stories we unravel. The shadows are beckoning, and the stories are about to unfold— don't miss out on this journey into the heart of horror.
           </Text>
           </Box>
         </VStack>
