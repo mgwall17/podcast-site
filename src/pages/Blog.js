1082bc7151d7060e8172fc6caa8024c5e3e89e1d
mgwall17 <mgwall17@gmail.com>
2023-11-29 21:22:26 -0700

    added a blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,14 +1,34 @@
 "use client";
-import { Link } from "@chakra-ui/next-js";
-import { Container, Heading, Text } from "@chakra-ui/react";
+import  Link  from "next/link";
+import { Container,Box,Text, Heading,Stack } from "@chakra-ui/react";
+
 export default function Blog() {
   return (
     <div>
       <Container>
-        <Heading>My Current Investigations</Heading>
-        <Text mt={2}>
-          Most Recent
-        </Text>
+        <Heading as="h1">Horror Glass Blog</Heading>
+        <Stack>
+        <Box>
+        <Link href="/Blog/Psycho-the-ultimate-classic">
+        <Box as="div" m={4} p={4} bgColor={'purple.800'} rounded={'10px'} _hover={{ bgColor: 'purple.600' }}>
+            <Heading size="lg" as="h2">Psycho, the Ultimate Classic
+            </Heading>
+          <Heading size="md" as="h3">September 3rd, 2023</Heading>
+          <Text as="i">By Jose Zaragoza</Text>
+        </Box>
+        </Link>
+        </Box>
+        <Box>
+        <Link href="/Blog/The-Thing-that-haunts-my-dreams">
+        <Box as="div" m={4} p={4} bgColor={'purple.800'} rounded={'10px'} _hover={{ bgColor: 'purple.600' }}>
+            <Heading size="lg" as="h2">The Thing that Haunts my Dreams
+            </Heading>
+          <Heading size="md" as="h3">October 12th, 2023</Heading>
+          <Text as="i">By Jose Zaragoza</Text>
+        </Box>
+        </Link>
+        </Box>
+        </Stack>
       </Container>
     </div>
   );
---
+++ src/pages/Blog/The-Thing-that-haunts-my-dreams.js
@@ -0,0 +1,39 @@
+/* eslint-disable react/no-unescaped-entities */
+"use client";
+import { Container, Heading, Text, Stack } from "@chakra-ui/react";
+export default function Thing() {
+  return (
+    <Container>
+      <Stack>
+        <Heading as="h1">The Thing that Haunts my Dreams</Heading>
+        <Heading size="md" as="h2">October 12th, 2023</Heading>
+        <Text as="i">By Jose Zaragoza</Text>
+        <Text>
+        There are few films that have left me as haunted and captivated as John Carpenter's "The Thing." Released in 1982, this cinematic masterpiece not only redefined the horror genre but also left an indelible mark on both cinema and popular culture.
+        </Text>
+        <Heading size="md" as="h3">A Cinematic Revolution</Heading>
+        <Text>
+        From the moment the ominous score by Ennio Morricone begins to play, "The Thing" sets a chilling tone that persists throughout its runtime. Carpenter's masterful direction, coupled with groundbreaking special effects by Rob Bottin, elevated the film to a level of visual and atmospheric horror previously unseen. The alien creature, with its grotesque transformations and unsettling mimicry, pushed the boundaries of practical effects and forever altered the landscape of creature features in cinema.
+        </Text>
+        <Heading size="md" as="h3">Cultural Impact</Heading>
+        <Text>
+        "The Thing" wasn't just a film; it was a cultural phenomenon that resonated far beyond the confines of the movie theater. Its exploration of paranoia and distrust in an isolated Antarctic outpost mirrored the anxieties of the Cold War era. The film's stark portrayal of a group unraveling under the threat of an unseen enemy struck a chord, making it a timeless reflection of societal fears.
+        </Text>
+        <Heading size="md" as="h3">Legacy in Horror Cinema</Heading>
+        <Text>
+        Decades later, "The Thing" remains a touchstone for horror filmmakers. Its influence can be seen in subsequent works that explore the psychological horror of isolation and the visceral terror of shape-shifting entities. The film's emphasis on suspense, practical effects, and atmospheric tension has become a blueprint for horror directors seeking to elicit genuine fear from their audiences.
+        </Text>
+        <Heading size="md" as="h3">Cult Following and Homages</Heading>
+        <Text>
+        "The Thing" has amassed a devoted cult following that celebrates its nuanced storytelling and groundbreaking effects. From video games to comic books, its legacy extends to various mediums. The film has been the subject of homage and reference in works across genres, showcasing its enduring impact on popular culture.
+
+        Most notably, the game Among Us that has our kids enraptured with mobile and PC gaming, is a direct homage to the film. The game's premise of a group of crewmates trying to identify an alien imposter is a direct reference to the film's plot. The game's popularity is a testament to the enduring legacy of "The Thing" and its ability to captivate audiences across generations.
+        </Text>
+        <Heading size="md" as="h3">A Frozen Classic</Heading>
+        <Text>
+        In reflecting on the impact of John Carpenter's "The Thing," it's evident that its influence goes beyond the horror genre. This frozen nightmare has left an indelible mark on cinema, inspiring filmmakers and captivating audiences with its visceral terror and psychological depth. As a horror enthusiast, I find myself returning to the frozen wastes of Antarctica, where the alien entity lurks, reshaping my understanding of fear and storytelling with each viewing. "The Thing" isn't just a film; it's a classic that continues to freeze the hearts of those willing to confront its chilling mysteries.
+        </Text>
+      </Stack>
+    </Container>
+  );
+}
