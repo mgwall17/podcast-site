2ff11f45e3e6770beee5f9c411413278646b8dc1
mgwall17 <mgwall17@gmail.com>
2024-03-15 23:35:56 -0700

    update

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -10,7 +10,7 @@ export default function Blog() {
         <Stack>
         <Box>
         <Link href="/Blog/Psycho-the-ultimate-classic">
-        <Box as="div" m={4} p={4} bgColor={'purple.800'} rounded={'10px'} _hover={{ bgColor: 'purple.600' }}>
+        <Box as="div" m={4} p={4} rounded={'10px'} _hover={{ bgColor: 'gray.100' }}>
             <Heading size="lg" as="h2">Psycho, the Ultimate Classic
             </Heading>
           <Heading size="md" as="h3">September 3rd, 2023</Heading>
@@ -20,7 +20,7 @@ export default function Blog() {
         </Box>
         <Box>
         <Link href="/Blog/The-Thing-that-haunts-my-dreams">
-        <Box as="div" m={4} p={4} bgColor={'purple.800'} rounded={'10px'} _hover={{ bgColor: 'purple.600' }}>
+        <Box as="div" m={4} p={4} rounded={'10px'} _hover={{ bgColor: 'gray.100' }}>
             <Heading size="lg" as="h2">The Thing that Haunts my Dreams
             </Heading>
           <Heading size="md" as="h3">October 12th, 2023</Heading>
@@ -30,7 +30,7 @@ export default function Blog() {
         </Box>
         <Box>
         <Link href="/Blog/Hellraiser-Hook-line-and-shiver">
-        <Box as="div" m={4} p={4} bgColor={'purple.800'} rounded={'10px'} _hover={{ bgColor: 'purple.600' }}>
+        <Box as="div" m={4} p={4} rounded={'10px'} _hover={{ bgColor: 'gray.100' }}>
             <Heading size="lg" as="h2">Hellraiser: Hook, Line and Shiver
             </Heading>
           <Heading size="md" as="h3">November 11th, 2023</Heading>
